3783d7f90d0838455af9e576ff8addae19df791f
Resul Dinc <dincresull@gmail.com>
2024-07-03 11:11:00 +0300

    create models

---
+++ ChatServiceV2/Context/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using ChatServiceV2.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChatServiceV2.Context
+{
+    public sealed class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Chat> Chats { get; set; }
+    }
+}
